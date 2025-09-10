c119f70d2c20583c824ea39b417b1eba8020af24
unknown <20mm313@esisalama.org>
2025-09-10 14:49:04 +0200

    blog slug

---
+++ app/(root)/blog/layout.tsx
@@ -0,0 +1,13 @@
+
+
+export default function BlogLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            {children}
+        </>
+    );
+}
