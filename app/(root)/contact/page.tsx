67844444fa985ac7068d1b351cefd3823ff0f10e
unknown <20mm313@esisalama.org>
2025-09-22 14:05:32 +0200

    map rized

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -200,7 +200,7 @@ const ContactPage = () => {
 
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d185.60688878469486!2d27.50439681050961!3d-11.660226088945041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19723f273e02596d%3A0x33d01072f53ab8fb!2s15%20Chau.%20de%20Kasenga%2C%20Lubumbashi!5e1!3m2!1sfr!2scd!4v1758536565460!5m2!1sfr!2scd"
-                    width="600"
+                    width="100%"
                     height="450"
                     style={{ border: 0 }}
                     // allowFullScreen=""
